b80c5854383749b4f3f16546ed1a44f2602ea03c
Thomas Bogle <thomasbogle@Thomass-MacBook-Pro.local>
2023-02-24 11:59:22 +0000

    Added two get requests for usersRouter

--- routes/usersRouter.js
+++ routes/usersRouter.js
@@ -2,27 +2,40 @@ const express = require('express');
 const {Sequelize} = require('sequelize');
 const usersRouter = express.Router();
 const users = require('../users.json');
-const shows = require('../shows.json');
+// const shows = require('../shows.json');
 
 // Read
 // All users
 usersRouter.get('/', (req, res) => {
     try {
-        if (users.length < 0) {
-            
+        if (users.usersData.length < 1) {
+            throw new Error("No user data available")
+        } else {
+            res.status(200).send({ msg: "Successfully requested all user data", users });
         }
-        res.status(200).send(users);
     } catch (error) {
-
+        res.status(400).send({ err: error.message })
     }
 })
 
 // One user
-usersRouter.get('', (req, res) => {
-    
+usersRouter.get('/:id', (req, res) => {
+    const userIndex = (req.params.id - 1)
+    try {
+        if (!users.usersData[userIndex]) {
+            throw new Error("No data available at this id number")
+        } else {
+            res.status(200).send({ msg: "Successfully requested user data", user: users.usersData[userIndex] })
+        }
+    } catch (error) {
+        res.status(400).send({ err: error.message })
+    }
 })
 
-// 
-usersRouter.get('', (req, res) => {
+// // 
+// usersRouter.get('', (req, res) => {
     
-})
+// })
+
+
+module.exports = usersRouter;
--- server.js
+++ server.js
@@ -1,12 +1,14 @@
 const express = require('express');
 const {db} = require('./db');
 const app = express();
+const usersRouter = require('./routes/usersRouter');
 
 const port = 3000;
 
 app.use(express.json())
 
-
+app.use('/users', usersRouter)
+app.use('/users/:id', usersRouter)
 
 
 app.listen(port, () => {
